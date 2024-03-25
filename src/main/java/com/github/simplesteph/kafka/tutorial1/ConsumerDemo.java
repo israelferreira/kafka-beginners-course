0cdaf31a89bc2cc179ff3391a23813f422f25cd3
Israel Ferreira <israelferreira5@outlook.com>
2024-03-25 17:44:20 -0300

    create a demo of a Kafka Consumer

---
+++ src/main/java/com/github/simplesteph/kafka/tutorial1/ConsumerDemo.java
@@ -0,0 +1,58 @@
+package com.github.simplesteph.kafka.tutorial1;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+public class ConsumerDemo {
+    private static final String BOOTSTRAP_SERVERS = "127.0.0.1:9092";
+    private static final Logger logger = LoggerFactory.getLogger(ConsumerDemo.class);
+
+    public static void main(String[] args) {
+
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(getProperties());
+
+        kafkaConsumer.subscribe(List.of("first_topic"));
+
+        while (true) {
+            ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(100));
+
+            for (ConsumerRecord<String, String> consumerRecord: records) {
+                logger.info("""
+                            Key: {}
+                            Value: {}
+                            Partition: {}
+                            Offset: {}
+                            Timestamp: {}
+                            """,
+                        consumerRecord.key(),
+                        consumerRecord.value(),
+                        consumerRecord.partition(),
+                        consumerRecord.offset(),
+                        consumerRecord.timestamp()
+                );
+            }
+        }
+    }
+
+    private static Properties getProperties() {
+        String groupId = "my-fourth-application";
+        var properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        return properties;
+    }
+
+}
