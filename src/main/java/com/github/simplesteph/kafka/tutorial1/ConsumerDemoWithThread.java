86c2913f98cab6c66a3820baf8e956095d16b9f3
Israel Ferreira <israelferreira5@outlook.com>
2024-03-26 18:18:47 -0300

    remove duplicated line

--- src/main/java/com/github/simplesteph/kafka/tutorial1/ConsumerDemoWithThread.java
+++ src/main/java/com/github/simplesteph/kafka/tutorial1/ConsumerDemoWithThread.java
@@ -58,7 +58,6 @@ public class ConsumerDemoWithThread {
             var properties = new Properties();
             properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
             properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-            properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
             properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
             properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
             properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
